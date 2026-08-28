edf6658ae017de1597e2e331202d68c95075b665
simon <hesynote@gmail.com>
2026-08-28 12:47:55 +0800

    set preview margin when hiding gutter in preview mode

--- MarkdownPreviewOverlay.py
+++ MarkdownPreviewOverlay.py
@@ -33,6 +33,7 @@ MODE_SETTING = "markdown_preview_overlay.preview_mode"
 STATUS_KEY = "markdown_preview_overlay"
 SETTINGS_NAME = "MarkdownPreviewOverlay.sublime-settings"
 SETTINGS_KEY = "markdown_preview_overlay.settings"
+PREVIEW_MARGIN = 16
 
 MARKDOWN_EXTENSIONS = {".md", ".markdown", ".mdown", ".mkd"}
 
@@ -79,6 +80,7 @@ class PreviewState(object):
         self.original_read_only = False
         self.original_gutter = True
         self.original_line_numbers = True
+        self.original_margin = None
         self.rendered_change_count = view.change_count()
         self.refresh_generation = 0
         self.control_generation = 0
@@ -235,6 +237,7 @@ class PreviewState(object):
         self.original_read_only = self.view.is_read_only()
         self.original_gutter = self.view.settings().get("gutter", True)
         self.original_line_numbers = self.view.settings().get("line_numbers", True)
+        self.original_margin = self.view.settings().get("margin")
         self.original_selections = _copy_regions(self.view.sel())
         self.original_viewport = self.view.viewport_position()
         self.original_folds = _copy_regions(self.view.folded_regions())
@@ -256,6 +259,7 @@ class PreviewState(object):
         if self._should_hide_line_numbers():
             self.view.settings().set("line_numbers", False)
             self.view.settings().set("gutter", False)
+            self.view.settings().set("margin", PREVIEW_MARGIN)
         self.view.set_read_only(True)
         self.render()
         self.view.set_viewport_position((0.0, 0.0), False)
@@ -282,6 +286,10 @@ class PreviewState(object):
         self.view.erase_status(STATUS_KEY)
         self.view.settings().set("gutter", self.original_gutter)
         self.view.settings().set("line_numbers", self.original_line_numbers)
+        if self.original_margin is not None:
+            self.view.settings().set("margin", self.original_margin)
+        else:
+            self.view.settings().erase("margin")
         self.view.set_read_only(self.original_read_only)
         self.render()
 
@@ -563,9 +571,14 @@ def _on_settings_change():
                     if state._should_hide_line_numbers():
                         view.settings().set("line_numbers", False)
                         view.settings().set("gutter", False)
+                        view.settings().set("margin", PREVIEW_MARGIN)
                     else:
                         view.settings().set("line_numbers", state.original_line_numbers)
                         view.settings().set("gutter", state.original_gutter)
+                        if state.original_margin is not None:
+                            view.settings().set("margin", state.original_margin)
+                        else:
+                            view.settings().erase("margin")
 
 
 def plugin_loaded():
